7c7cc8ee39ca623223eb31fa1f8e372868fb59ee
sojebsikder <sojebsikder@gmail.com>
2021-02-14 14:53:10 +0600

    main

---
+++ resources/js/components/Index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Index() {
+    return (
+        <div>
+            <h2>We will make Question Answer app</h2>
+        </div>
+    );
+}
+
+export default Index;
